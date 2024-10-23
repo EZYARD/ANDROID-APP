ca25ba53cf171c81ebefed87016d2dbe2ada99a6
irvin1226 <IrvinOrtiz805@gmail.com>
2024-10-22 17:53:47 -0700

    changed several card settings

--- app/src/main/java/com/example/sprint0backend/Constants.kt
+++ app/src/main/java/com/example/sprint0backend/Constants.kt
@@ -1,5 +1,7 @@
 package com.example.sprint0backend
 
 data class Constants(
-    val BACKEND_URL: String = "http://ec2-54-81-96-1.compute-1.amazonaws.com:8000/"
+    val BACKEND_URL: String = "http://10.0.2.2:8000/"
 )
+// Connect to local host http://10.0.2.2:8000/
+// Connect to AWS http://ec2-54-81-96-1.compute-1.amazonaws.com:8000/
