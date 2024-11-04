db521df3b8bee3b7ab3a378134d48e9c9bb0cb54
kiwi0401 <kiwi04011@gmail.com>
2024-11-04 09:25:11 -0800

    remove secret

--- app/src/main/java/com/example/sprint0backend/Constants.kt
+++ app/src/main/java/com/example/sprint0backend/Constants.kt
@@ -2,6 +2,5 @@ package com.example.sprint0backend
 
 data class Constants(
     val BACKEND_URL: String = "http://10.0.2.2:8000/",
-    val GOOGLE_API_KEY: String = "AIzaSyC8INpyaAj27jNOFiyWh413zBzmNYSPGHM"
 )// local host: "http://10.0.2.2:8000/"
 // AWS: "http://ec2-54-81-96-1.compute-1.amazonaws.com:8000/"
